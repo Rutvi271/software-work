9274b1f2b03435ab537be2d8bf931314e833094a
Rutvi271 <rutvi271@gmail.com>
2023-12-03 04:38:20 +0000

    aa

---
+++ com-herokuapp-sw-2/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+P
+ src/browserfactory/BaseTest.java,7\d\7d6bfbe9f5ec09fad00908624277ff4f57f20ae4
+L
+src/testsuite/LoginTest.java,7\1\71ba56529a3d3927dbdffc2b6e5d28fdfaf2d287
--- com-herokuapp-sw-2/src/testsuite/LoginTest.java
+++ com-herokuapp-sw-2/src/testsuite/LoginTest.java
@@ -1,2 +1,53 @@
-package testsuite;public class LoginTest {
+package testsuite;
+
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class LoginTest extends BaseTest {
+    String baseUrl="http://the-internet.herokuapp.com/login";
+    @Before
+    public void open(){
+        openBrowser(baseUrl);
+
+
+    }
+    @Test
+    public void UserSholdLoginSuccessfullyWithValidCredentials(){
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.xpath("//input[contains(@name,'password')]")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//button[contains(@class,'radius')]")).click();
+        String ActualText=driver.findElement(By.xpath("//div[@class='example']/h2")).getText();
+        String ExpectedText="Secure Area";
+        Assert.assertEquals(ActualText,ExpectedText);
+    }
+
+    @Test
+    public void verifyTheUsernameErrorMessage()
+    {
+        driver.findElement(By.id("username")).sendKeys("tomsmith1");
+        driver.findElement(By.xpath("//input[contains(@name,'password')]")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//button[contains(@class,'radius')]")).click();
+        String ActualText=driver.findElement(By.xpath("//div[@class='flash error']")).getText();
+        String ExpectedText="Your username is invalid!";
+        Assert.assertEquals(ActualText,ExpectedText);
+    }
+    @Test
+    public void verifyThePasswordErrorMessage()
+    {
+        driver.findElement(By.id("username")).sendKeys("tomsmith1");
+        driver.findElement(By.xpath("//input[contains(@name,'password')]")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//button[contains(@class,'radius')]")).click();
+        String ActualText=driver.findElement(By.xpath("//div[@class='flash error']")).getText();
+        String ExpectedText="Your password is invalid!";
+        Assert.assertEquals(ActualText,ExpectedText);
+    }
+    @After
+    public void close(){
+        closeBrowser();
+    }
+
 }
